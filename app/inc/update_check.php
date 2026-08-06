f786ce4b5ef2f9b41c39ecd0d19b30190b790bb3
FrazoN11 <fkryszon@gmail.com>
2026-08-06 12:39:59 +0200

    Bump application version to 0.6.20.5

--- app/inc/update_check.php
+++ app/inc/update_check.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const OPNCENTRAL_VERSION = '0.6.20.4';
+const OPNCENTRAL_VERSION = '0.6.20.5';
 const OPNCENTRAL_GITHUB_REPOSITORY = 'frazon11/opnSentral';
 const OPNCENTRAL_UPDATE_INTERVAL = 86400;
 
