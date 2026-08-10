8eee18e35a122e0bc2aaa818a28d380c9fe321a9
FrazoN11 <fkryszon@gmail.com>
2026-08-10 13:15:42 +0200

    Bump version to 0.6.20.20

--- app/inc/update_check.php
+++ app/inc/update_check.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const OPNSENTRAL_VERSION = '0.6.20.19';
+const OPNSENTRAL_VERSION = '0.6.20.20';
 const OPNSENTRAL_GITHUB_REPOSITORY = 'frazon11/opnSentral';
 const OPNSENTRAL_UPDATE_INTERVAL = 86400;
 
