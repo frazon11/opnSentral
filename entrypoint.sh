010464f9423f2e17d58c710a477b45e0acc8bc70
FrazoN11 <fkryszon@gmail.com>
2026-08-10 09:04:17 +0200

    Send telemetry on opnSentral container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -eu
+
 mkdir -p /var/www/data /var/www/backups
 chown -R www-data:www-data /var/www/data /var/www/backups
 
 su -s /bin/sh www-data -c 'php /var/www/html/alert_worker.php' &
+
+(
+    sleep 2
+    su -s /bin/sh www-data -c 'php /var/www/html/telemetry_startup.php'
+) &
 
 exec "$@"
