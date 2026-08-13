ab73a8a49bade8ff8392776063c1aedf4e1889b9
FrazoN11 <fkryszon@gmail.com>
2026-08-13 08:13:12 +0200

    Load static sidebar collapse behavior

--- app/inc/footer.php
+++ app/inc/footer.php
@@ -3,6 +3,7 @@
 <footer class="app-footer"><?= h(t('footer')) ?></footer>
 <link rel="stylesheet" href="/assets/topbar-controls.css?v=06215">
 <link rel="stylesheet" href="/assets/ids-bulk-actions.css?v=06215">
+<script src="/assets/sidebar-collapse.js?v=062117"></script>
 <script src="/assets/sidebar-scroll.js?v=062116"></script>
 <script src="/assets/ids-write-label.js?v=06215"></script>
 <script src="/assets/ids-bulk-actions.js?v=06215"></script>
