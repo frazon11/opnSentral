72fe24f82d5709c34ec52f0cf753fedaaa86d516
FrazoN11 <fkryszon@gmail.com>
2026-08-22 19:07:57 +0200

    Bump opnSentral to 0.6.21.48

--- app/inc/update_check.php
+++ app/inc/update_check.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const OPNSENTRAL_VERSION = '0.6.21.47';
+const OPNSENTRAL_VERSION = '0.6.21.48';
 const OPNSENTRAL_GITHUB_REPOSITORY = 'frazon11/opnSentral';
 const OPNSENTRAL_UPDATE_INTERVAL = 86400;
 
