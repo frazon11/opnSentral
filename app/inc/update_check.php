883d3aefa413b3018ada028d6c4182e16e81c348
FrazoN11 <fkryszon@gmail.com>
2026-08-31 22:43:16 +0200

    Release 0.6.21.85 dashboard hardware fixes

--- app/inc/update_check.php
+++ app/inc/update_check.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-const OPNSENTRAL_VERSION = '0.6.21.84';
+const OPNSENTRAL_VERSION = '0.6.21.85';
 const OPNSENTRAL_GITHUB_REPOSITORY = 'frazon11/opnSentral';
 const OPNSENTRAL_UPDATE_INTERVAL = 86400;
 
